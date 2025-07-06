f5c0e79f6f3cbc3fb7aa9f274cce62b4f3337233
YgorNogue <ygor.nogueira@hotmail.com>
2025-07-05 22:57:54 -0300

    Create carrinho.js

---
+++ carrinho.js
@@ -0,0 +1,6 @@
+function adicionarAoCarrinho(nome, preco) {
+  const carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+  carrinho.push({ nome, preco });
+  localStorage.setItem('carrinho', JSON.stringify(carrinho));
+  alert(`${nome} adicionado ao carrinho!`);
+}
